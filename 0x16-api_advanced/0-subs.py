4f649e8869d6a19a208e1ae727e50ad3819917f3
henry <hjowino@gmail.com>
2024-06-05 21:52:44 +0300

    updated the return function

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,28 +1,25 @@
-#!/usr/bin/python3
-"""
-Script that queries subscribers on a given Reddit subreddit.
-"""
-
 import requests
-import sys
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
+    
     if response.status_code == 200:
         data = response.json()
-        subscribers = data['data']['subscribers']
+        subscribers = data.get('data', {}).get('subscribers', 0)
         return subscribers
     else:
         return 0
 
 if __name__ == "__main__":
-    if len(sys.argv)!= 2:
+    import sys
+    
+    if len(sys.argv) != 2:
         print("Usage: python3 sub.py <subreddit>")
         sys.exit(1)
 
     subreddit = sys.argv[1]
     subscribers = number_of_subscribers(subreddit)
-    print("Subscribers on {}: {}".format(subreddit, subscribers))
+    print(f"Subscribers on {subreddit}: {subscribers}")
