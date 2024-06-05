ce426005b7870adf290e42d7a3a69b386306e684
henry <hjowino@gmail.com>
2024-06-05 22:11:19 +0300

    updated the return function

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,28 +1,27 @@
 #!/usr/bin/python3
 """
-Script that queries subscribers on a given Reddit subreddit.
+Importing requests module
 """
 
-import requests
-import sys
+from requests import get
+
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}  # Custom User-Agent
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
+    """
+    function that queries the Reddit API and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit.
+    """
+
+    if subreddit is None or not isinstance(subreddit, str):
         return 0
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 sub.py <subreddit>")
-        sys.exit(1)
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = get(url, headers=user_agent)
+    all_data = response.json()
 
-    subreddit = sys.argv[1]
-    subscribers = number_of_subscribers(subreddit)
-    print("Subscribers on {}: {}".format(subreddit, subscribers))
+    try:
+        return all_data.get('data').get('subscribers')
+
+    except:
+        return 0
