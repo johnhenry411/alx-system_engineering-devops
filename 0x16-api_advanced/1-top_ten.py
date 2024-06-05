9ef3d687054d67588e4300c69bb895b4d5604094
henry <hjowino@gmail.com>
2024-06-05 22:03:55 +0300

    updated the api responce

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,28 +1,42 @@
 #!/usr/bin/python3
 """
-Script that queries subscribers on a given Reddit subreddit.
+Script to print hot posts on a given Reddit subreddit.
 """
 
 import requests
 import sys
 
-def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}  # Custom User-Agent
-    response = requests.get(url, headers=headers, allow_redirects=False)
+def top_ten(subreddit):
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    # Construct the URL for the subreddit's hot posts in JSON format
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+
+    params = {
+        "limit": 10
+    }
+
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+
     if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
+        results = response.json().get("data")
+        if results:
+            print("Hot posts on r/{}:".format(subreddit))
+            for post in results.get("children"):
+                print(post.get("data").get("title"))
+        else:
+            print(None)
     else:
-        return 0
+        print(None)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 sub.py <subreddit>")
+    if len(sys.argv)!= 2:
+        print("Usage: python3 hot_posts.py <subreddit>")
         sys.exit(1)
 
     subreddit = sys.argv[1]
-    subscribers = number_of_subscribers(subreddit)
-    print("Subscribers on {}: {}".format(subreddit, subscribers))
+    top_ten(subreddit)
